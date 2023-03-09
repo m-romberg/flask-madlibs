314cabbf01ce336a28ba58e1c7e3a061f2ab7c34
Justin Clark <jclarksummit@gmail.com>
2023-03-08 20:11:33 -0500

    Finished part 1

--- app.py
+++ app.py
@@ -8,8 +8,19 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+
 @app.get('/')
 def index():
     """Return Homepage of Form"""
     prompts = silly_story.prompts
-    return render_template("questions.html", prompts = prompts)
+    return render_template("questions.html", prompts=prompts)
+
+# Display results on screen
+
+
+@app.get('/results')
+def generate_story():
+    """Takes input from questions & returns the story"""
+    inputs = request.args
+    story_string = silly_story.generate(inputs)
+    return render_template("results.html", story_string=story_string)
