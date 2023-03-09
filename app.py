c964bffcb506417a8c37fc4488ad04936ff9c1d2
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-03-08 16:19:53 -0800

    add inputs for prompts

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/')
+def index():
+    """Return Homepage of Form"""
+    prompts = silly_story.prompts
+    return render_template("questions.html", prompts = prompts)
